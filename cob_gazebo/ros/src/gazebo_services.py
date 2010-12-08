9557764bb90f266819b9f64f3d4bd812fb189d18
ipa-fmw <fmw@ipa.fhg.de>
2010-12-08 12:39:28 +0100

    preparing release

--- cob_gazebo/ros/src/gazebo_services.py
+++ cob_gazebo/ros/src/gazebo_services.py
@@ -3,71 +3,111 @@ import roslib; roslib.load_manifest('cob_gazebo')
 
 import rospy
 import tf
+import actionlib
 
-from pr2_controllers_msgs.msg import JointTrajectoryControllerState
-from simple_script_server import *
-
+from pr2_controllers_msgs.msg import JointTrajectoryAction
+from move_base_msgs.msg import MoveBaseAction
 
 # care-o-bot includes
-from cob_msgs.msg import *
 from cob_srvs.srv import *
 
 class gazebo_services():
 
 	def __init__(self):
-		self.sss = simple_script_server()
-
-		#tray
-		self.tray_states = JointTrajectoryControllerState()
-		self.tray_init_srv = rospy.Service('/tray_controller/init', Trigger, self.tray_init_cb)
-		self.tray_stop_srv = rospy.Service('/tray_controller/stop', Trigger, self.tray_stop_cb)
-		self.tray_recover_srv = rospy.Service('/tray_controller/recover', Trigger, self.tray_recover_cb)
-		self.tray_set_operation_mode_srv = rospy.Service('/tray_controller/set_operation_mode', SetOperationMode, self.tray_set_operation_mode_cb)
-		self.tray_js_sub = rospy.Subscriber("/tray_controller/state", JointTrajectoryControllerState, self.tray_js_cb)
+		# base
+		self.base_client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+		self.base_init_srv = rospy.Service('/base_controller/init', Trigger, self.base_init_cb)
+		self.base_stop_srv = rospy.Service('/base_controller/stop', Trigger, self.base_stop_cb)
+		self.base_recover_srv = rospy.Service('/base_controller/recover', Trigger, self.base_recover_cb)
+		self.base_set_operation_mode_srv = rospy.Service('/base_controller/set_operation_mode', SetOperationMode, self.base_set_operation_mode_cb)
 
 		#torso
-		self.torso_states = JointTrajectoryControllerState()
+		self.torso_client = actionlib.SimpleActionClient('/torso_controller/joint_trajectory_action', JointTrajectoryAction)
 		self.torso_init_srv = rospy.Service('/torso_controller/init', Trigger, self.torso_init_cb)
 		self.torso_stop_srv = rospy.Service('/torso_controller/stop', Trigger, self.torso_stop_cb)
 		self.torso_recover_srv = rospy.Service('/torso_controller/recover', Trigger, self.torso_recover_cb)
 		self.torso_set_operation_mode_srv = rospy.Service('/torso_controller/set_operation_mode', SetOperationMode, self.torso_set_operation_mode_cb)
-		self.torso_js_sub = rospy.Subscriber("/torso_controller/state", JointTrajectoryControllerState, self.torso_js_cb)
 
-		#head
-		self.head_states = JointTrajectoryControllerState()
-		self.head_init_srv = rospy.Service('/head_controller/init', Trigger, self.head_init_cb)
-		self.head_stop_srv = rospy.Service('/head_controller/stop', Trigger, self.head_stop_cb)
-		self.head_recover_srv = rospy.Service('/head_controller/recover', Trigger, self.head_recover_cb)
-		self.head_set_operation_mode_srv = rospy.Service('/head_controller/set_operation_mode', SetOperationMode, self.head_set_operation_mode_cb)
-		self.head_js_sub = rospy.Subscriber("/head_controller/state", JointTrajectoryControllerState, self.head_js_cb)
-
-		#sdh
-		self.sdh_states = JointTrajectoryControllerState()
-		self.sdh_init_srv = rospy.Service('/sdh_controller/init', Trigger, self.sdh_init_cb)
-		self.sdh_stop_srv = rospy.Service('/sdh_controller/stop', Trigger, self.sdh_stop_cb)
-		self.sdh_recover_srv = rospy.Service('/sdh_controller/recover', Trigger, self.sdh_recover_cb)
-		self.sdh_set_operation_mode_srv = rospy.Service('/sdh_controller/set_operation_mode', SetOperationMode, self.sdh_set_operation_mode_cb)
-		self.sdh_js_sub = rospy.Subscriber("/sdh_controller/state", JointTrajectoryControllerState, self.sdh_js_cb)
+		#tray
+		self.tray_client = actionlib.SimpleActionClient('/tray_controller/joint_trajectory_action', JointTrajectoryAction)
+		self.tray_init_srv = rospy.Service('/tray_controller/init', Trigger, self.tray_init_cb)
+		self.tray_stop_srv = rospy.Service('/tray_controller/stop', Trigger, self.tray_stop_cb)
+		self.tray_recover_srv = rospy.Service('/tray_controller/recover', Trigger, self.tray_recover_cb)
+		self.tray_set_operation_mode_srv = rospy.Service('/tray_controller/set_operation_mode', SetOperationMode, self.tray_set_operation_mode_cb)
 
 		#arm
-		self.arm_states = JointTrajectoryControllerState()
+		self.arm_client = actionlib.SimpleActionClient('/arm_controller/joint_trajectory_action', JointTrajectoryAction)
 		self.arm_init_srv = rospy.Service('/arm_controller/init', Trigger, self.arm_init_cb)
 		self.arm_stop_srv = rospy.Service('/arm_controller/stop', Trigger, self.arm_stop_cb)
 		self.arm_recover_srv = rospy.Service('/arm_controller/recover', Trigger, self.arm_recover_cb)
 		self.arm_set_operation_mode_srv = rospy.Service('/arm_controller/set_operation_mode', SetOperationMode, self.arm_set_operation_mode_cb)
-		self.arm_js_sub = rospy.Subscriber("/arm_controller/state", JointTrajectoryControllerState, self.arm_js_cb)
 
-	# tray
+		#sdh
+		self.sdh_client = actionlib.SimpleActionClient('/sdh_controller/joint_trajectory_action', JointTrajectoryAction)
+		self.sdh_init_srv = rospy.Service('/sdh_controller/init', Trigger, self.sdh_init_cb)
+		self.sdh_stop_srv = rospy.Service('/sdh_controller/stop', Trigger, self.sdh_stop_cb)
+		self.sdh_recover_srv = rospy.Service('/sdh_controller/recover', Trigger, self.sdh_recover_cb)
+		self.sdh_set_operation_mode_srv = rospy.Service('/sdh_controller/set_operation_mode', SetOperationMode, self.sdh_set_operation_mode_cb)
+		
+		#head
+		self.head_client = actionlib.SimpleActionClient('/head_controller/joint_trajectory_action', JointTrajectoryAction)
+		self.head_init_srv = rospy.Service('/head_controller/init', Trigger, self.head_init_cb)
+		self.head_stop_srv = rospy.Service('/head_controller/stop', Trigger, self.head_stop_cb)
+		self.head_recover_srv = rospy.Service('/head_controller/recover', Trigger, self.head_recover_cb)
+		self.head_set_operation_mode_srv = rospy.Service('/head_controller/set_operation_mode', SetOperationMode, self.head_set_operation_mode_cb)
+
+	# base
+	def base_init_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def base_stop_cb(self, req):
+		self.base_client.cancel_all_goals()
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+
+	def base_recover_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def base_set_operation_mode_cb(self, req):
+		resp = SetOperationModeResponse()
+		resp.success.data = True
+		return resp
+
+	# torso
+	def torso_init_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def torso_stop_cb(self, req):
+		self.torso_client.cancel_all_goals()
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+
+	def torso_recover_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def torso_set_operation_mode_cb(self, req):
+		resp = SetOperationModeResponse()
+		resp.success.data = True
+		return resp
+
+	#tray
 	def tray_init_cb(self, req):
 		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
 	
 	def tray_stop_cb(self, req):
-		self.tray_goal = []
-		for i in range(len(self.tray_states.actual.positions)):
-			self.tray_goal.append(self.tray_states.actual.positions[i])
-		self.sss.move("tray",[self.tray_goal])
+		self.tray_client.cancel_all_goals()
 		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
@@ -77,45 +117,63 @@ class gazebo_services():
 		resp.success.data = True
 		return resp
 	
-
 	def tray_set_operation_mode_cb(self, req):
 		resp = SetOperationModeResponse()
 		resp.success.data = True
 		return resp
 
-	def tray_js_cb(self, data):
-		self.tray_states=data
-
-	#torso
-	def torso_init_cb(self, req):
+	# arm
+	def arm_init_cb(self, req):
 		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
 	
-	def torso_stop_cb(self, req):
-		self.torso_goal = []
-		for i in range(len(self.torso_states.actual.positions)):
-			self.torso_goal.append(self.torso_states.actual.positions[i])
-		self.sss.move("torso",[self.torso_goal])
+	def arm_stop_cb(self, req):
+		self.arm_client.cancel_all_goals()
 		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
 
-	def torso_recover_cb(self, req):
+	def arm_recover_cb(self, req):
 		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
-
-	def torso_set_operation_mode_cb(self, req):
+	
+	def arm_set_operation_mode_cb(self, req):
 		resp = SetOperationModeResponse()
 		resp.success.data = True
 		return resp
 
-	def torso_js_cb(self, data):
-		self.torso_states=data
+	# sdh
+	def sdh_init_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def sdh_stop_cb(self, req):
+		self.sdh_client.cancel_all_goals()
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+
+	def sdh_recover_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def sdh_set_operation_mode_cb(self, req):
+		resp = SetOperationModeResponse()
+		resp.success.data = True
+		return resp
 
 	# head
 	def head_init_cb(self, req):
+		resp = TriggerResponse()
+		resp.success.data = True
+		return resp
+	
+	def head_stop_cb(self, req):
+		self.head_client.cancel_all_goals()
 		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
@@ -125,82 +183,8 @@ class gazebo_services():
 		resp.success.data = True
 		return resp
 	
-	def head_stop_cb(self, req):
-		self.head_goal = []
-		for i in range(len(self.head_states.actual.positions)):
-			self.head_goal.append(self.head_states.actual.positions[i])
-		self.sss.move("head",[self.head_goal])
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-
 	def head_set_operation_mode_cb(self, req):
 		resp = SetOperationModeResponse()
-		resp.success.data = True
-		return resp
-
-	def head_js_cb(self, data):
-		self.head_states=data
-
-	# sdh
-	def sdh_init_cb(self, req):
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-	
-	def sdh_stop_cb(self, req):
-		self.sdh_goal = []
-		for i in range(len(self.sdh_states.actual.positions)):
-			self.sdh_goal.append(self.sdh_states.actual.positions[i])
-		self.sss.move("sdh",[self.sdh_goal])
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-
-	def sdh_recover_cb(self, req):
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-
-	def sdh_set_operation_mode_cb(self, req):
-		resp = SetOperationModeResponse()
-		resp.success.data = True
-		return resp
-
-	def sdh_js_cb(self, data):
-		self.sdh_states=data
-
-	# arm
-	def arm_init_cb(self, req):
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-	
-	def arm_stop_cb(self, req):
-		self.arm_goal = []
-		for i in range(len(self.arm_states.actual.positions)):
-			self.arm_goal.append(self.arm_states.actual.positions[i])
-		self.sss.move("arm",[self.arm_goal],False)
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-
-	def arm_recover_cb(self, req):
-		resp = TriggerResponse()
-		resp.success.data = True
-		return resp
-
-	def arm_set_operation_mode_cb(self, req):
-		resp = SetOperationModeResponse()
-		resp.success.data = True
-		return resp
-
-	def arm_js_cb(self, data):
-		self.arm_states=data
-
-	# base
-	def base_stop_cb(self, req):
-		resp = TriggerResponse()
 		resp.success.data = True
 		return resp
 
