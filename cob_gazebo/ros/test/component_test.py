14089a2cf15477ed2e9b6a8e2be48d2af3833f38
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2011-02-06 17:23:29 +0100

    modified unittest for components

--- cob_gazebo/ros/test/component_test.py
+++ cob_gazebo/ros/test/component_test.py
@@ -27,69 +27,95 @@ class UnitTest(unittest.TestCase):
     def test_unit(self):
         # fetch parameters
         try:
-            # time of test
-            test_duration = float(rospy.get_param('~test_duration'))
             # error range
-            error_range = float(rospy.get_param('~error_range'))      
+            if not rospy.has_param('~error_range'):
+                self.fail('parameter error_range does not exist on ROS Parameter Server')
+            error_range = rospy.get_param('~error_range')
+
             # time to wait before
-            wait_time = rospy.get_param('~wait_time')             
-            # topics 
-            command_topic = rospy.get_param('~command_topic')
-            state_topic = rospy.get_param('~state_topic')
-            # part of robot 
+            if not rospy.has_param('~wait_time'):
+                self.fail('parameter wait_time does not exist on ROS Parameter Server')
+            wait_time = rospy.get_param('~wait_time')
+            
+            # part of robot
+            if not rospy.has_param('~component'):
+                self.fail('parameter component does not exist on ROS Parameter Server')
             component = rospy.get_param('~component')
+            
+            # command topic
+            if not rospy.has_param('~command_topic'):
+                self.fail('parameter command_topic does not exist on ROS Parameter Server')
+            command_topic = rospy.get_param('~command_topic')
+            
+            # state topic
+            if not rospy.has_param('~state_topic'):
+                self.fail('parameter state_topic does not exist on ROS Parameter Server')
+            state_topic = rospy.get_param('~state_topic')
+            
             # movement command
+            if not rospy.has_param('~target'):
+                self.fail('parameter target does not exist on ROS Parameter Server')
             target = rospy.get_param('~target')
         except KeyError, e:
-            self.fail('cobunit not initialized properly')
+            self.fail('Parameters not set properly')
         print """
               Test: %s  
-              Movement: %s
-              Command: %s
-              State: %s
-              Test Duration: %s
+              Target: %s
+              Command topic: %s
+              State topic: %s
               Error Range: %s
-              Wait Time: %s"""%(component, target, command_topic, state_topic, test_duration, error_range, wait_time)
-        self._test_cob(component, command_topic, state_topic, test_duration, error_range, wait_time, target)
+              Wait Time: %s"""%(component, target, command_topic, state_topic, error_range, wait_time)
         
-    def _test_cob(self,component, command_topic, state_topic, test_duration, error_range, wait_time, target): 
-        self.assert_(test_duration > 0.0, "bad parameter (test_duration)")
-        self.sss.init(component)
+        # check parameters
+        # \todo do parameter tests
+        #self.assert_(test_duration > 0.0, "bad parameter (test_duration)")
         
-        # start actual test
+        # init subscribers
         sub_command_topic = rospy.Subscriber(command_topic, JointTrajectory, self.cb_command) 
         sub_state_topic = rospy.Subscriber(state_topic, JointTrajectoryControllerState, self.cb_state) 
+        
+        # init component
+        init_handle = self.sss.init(component)
+        if init_handle.get_error_code() != 0:
+            error_msg = 'Could not initialize ' + component
+            self.fail(error_msg)
+        
+        # start actual test
         print "Waiting for messages"
-        # give the test some seconds to start, may parameterize this in the future
+        # give the topics some seconds to receive messages
         wallclock_timeout_t = time.time() + wait_time
         while not self.message_received and time.time() < wallclock_timeout_t:
             #print "###debug here###" 
             time.sleep(0.1)
+        if not self.message_received:
+            self.fail('No state message received within wait_time')
           
-        # send commands to cob
-        self.sss.move(component,target)
+        # send commands to component
+        #move_handle = self.sss.move(component,target)
+        move_handle = self.sss.move("arm","folded")
+        if move_handle.get_error_code() != 0:
+            error_msg = 'Could not move ' + component
+            self.fail(error_msg)
+        
+        # get last point out of trajectory
+        traj_endpoint = self.command_traj.points[len(self.command_traj.points)-1]
              
-        print "Starting measurement"
-        timeout_t = rospy.get_time() + test_duration
-        while rospy.get_time() < timeout_t:
-            rospy.sleep(0.1)
+        # Start evaluation
+        timeout_t = traj_endpoint.time_from_start.to_sec()*0.5 # movement should already be finished, but let wait with an additional buffer of 50% times the desired time
+        rospy.sleep(timeout_t)
         print "Done waiting, validating results"
-        print self.actual_pos
-        # checking here
-        i = 0
-        while i < len(self.comm_pos):  
-            self.assert_(((self.comm_pos[i] - self.actual_pos[i]) < error_range), "Positive Range Error happens")
-            self.assert_(((self.comm_pos[i] - self.actual_pos[i]) > -error_range), "Negative Range Error happens")
-            i = i + 1
+        actual_pos = self.actual_pos # fix current position configuration for later evaluation
+        
+        # checking if target position is realy reached
+        for i in range(len(traj_endpoint.positions)):
+            self.assert_(((traj_endpoint.positions[i] - actual_pos[i]) < error_range), "Target position out of error_range")
             
-    # call back functions
+    # callback functions
     def cb_state(self, msg):
         self.message_received = True
         self.actual_pos = msg.actual.positions
     def cb_command(self, msg):
-        self.comm_pos = msg.points[0].positions
-        #print "### command callback!!!###"
-        #print self.comm_pos
+        self.command_traj = msg
          
 if __name__ == '__main__':
     try:
