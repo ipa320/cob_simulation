75eeb8a3a0051ad9af2a7ebb04cf0d8b3f1dc6c8
ipa-nhg <nadia.hammoudeh-garcia@ipa.fraunhofer.de>
2012-06-29 10:43:32 +0200

    Added elevators in ipa-apartment world

--- cob_gazebo_worlds/ros/src/elevator.py
+++ cob_gazebo_worlds/ros/src/elevator.py
@@ -5,7 +5,7 @@ import sys
 import roslib
 roslib.load_manifest('cob_gazebo_worlds')
 import rospy
-
+import random
 from math import *
 
 from gazebo.srv import *
@@ -14,54 +14,57 @@ from gazebo_msgs.msg import *
 
 
 apply_effort_service = rospy.ServiceProxy('/gazebo/apply_joint_effort', ApplyJointEffort)
-door1_closed = True
+door_closed = True
 
 
-def callback():
-    rand = (numpy.random.randint(0,size=1))
-    if rand:
-    	move_door("left")
-    else:
-    	move_door("right")
+
+def callback(ContactsState):
+
+	if door_closed:
+		if (ContactsState.states != []):
+			rospy.loginfo("button pressed")	
+			rand = (random.randint(0,1))
+			if rand == 0:
+				move_door("left")
+			else:
+				move_door("right")
+		else:
+			rospy.logdebug("button not pressed")
+	else:
+		rospy.loginfo("Door Opened")
+
+
 
 def listener():
+    
     rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/elevator_button1_bumper/state", ContactsState, callback)	
-
-    # add default effort to door
-    req = ApplyJointEffortRequest()
-    req.joint_name = 'joint_elevator_left'
-    req.effort = -500
-    req.start_time.secs = 0
-    req.duration.secs = -10
-    res = apply_effort_service(req)
-
+    rospy.Subscriber("/elevator_button1_bumper/state", ContactsState, callback, queue_size=1)
     rospy.spin()
 
 def move_door(side):
 
+	door_closed = False
 	req = ApplyJointEffortRequest()
 	req.joint_name = 'joint_elevator_'+side
 	req.start_time.secs = 0
 	req.duration.secs = -10
-	req.effort = 1000
-	door_closed = True
+	req.effort = 500
 	rospy.loginfo("door is opening")
 	res = apply_effort_service(req)
 
-	rospy.sleep(30)
-
+	rospy.sleep(10)
 	req.effort = -1000
-	door_closed = False
 	rospy.loginfo("door is closing")
 	res = apply_effort_service(req)
 
+	rospy.sleep(10)
+	req.effort = 500
+	res = apply_effort_service(req)
+	door_closed = True
 
 
 if __name__ == '__main__':
     listener()
-    #move_door("right")
-
 
 
 
