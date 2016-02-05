1c0b3b3066b74dc608051d0d4c832f7eda5c5a33
Stephan Dittmann <stephan.dittmann@ipa.fraunhofer.de>
2016-02-05 14:03:39 +0100

    Edited the script to make it possible to hand over arguments

--- cob_bringup_sim/scripts/delay_robot_spawn.sh
+++ cob_bringup_sim/scripts/delay_robot_spawn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-roslaunch cob_bringup_sim gazebo_world.launch &
+roslaunch cob_bringup_sim gazebo_world.launch $@ &
 sleep 5
-roslaunch cob_bringup_sim gazebo_robot.launch 
+roslaunch cob_bringup_sim gazebo_robot.launch $@
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
